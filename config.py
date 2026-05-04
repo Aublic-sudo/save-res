54492cfbf2ff3f6bcb8f303c39d5e6dd50320a7a
Aublic-sudo <rstock937@gmail.com>
2026-05-04 19:55:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
@@ -25,8 +24,8 @@ MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1003727821596")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-1003727821596")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1003675139654")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1003789088306")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
@@ -35,7 +34,6 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "99999999"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/forssub") # this link for start command message#
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/RixieHQ")
-
 
 
 
